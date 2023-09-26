6abe8e5e874da95e0ba7fe9922c7bae81b27f02c
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-09-26 19:37:30 +0200

    router auth

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,29 @@
+import { createUser, getUserByEmail } from "../db/users";
+import { random, authentication } from "../helpers";
+import express from "express";
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+    const existUser = await getUserByEmail(email);
+
+    if (existUser) {
+      return res.sendStatus(400);
+    }
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+    return res.sendStatus(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
