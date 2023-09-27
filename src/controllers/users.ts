1f1053b1773b57851441e1702f42fd5fe07f9ee2
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-09-27 16:52:26 +0200

    create update method

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,25 @@
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 import express from "express";
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.body.params;
+    const { username } = req.body;
+    if (!username) {
+      return res.sendStatus(400);
+    }
+    const user = await getUserById(id);
+    user.username = username;
+    await user.save();
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
 
 export const getAllUsers = async (
   req: express.Request,
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { deleteUser, getAllUsers } from "../controllers/users";
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
 import { isAunthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
   router.get("/users", isAunthenticated, getAllUsers);
-  router.delete("users/:id", isOwner, deleteUser);
+  router.delete("users/:id", isAunthenticated, isOwner, deleteUser);
+  router.patch("user/:id", isAunthenticated, isOwner, updateUser);
 };
